c92218f950292c1ec0462a31935e9eecbaa64528
100Gyeon <bjy9966@naver.com>
2021-11-14 23:33:12 +0900

    chore: 개발 환경 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,19 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "prettier/prettier": ["error", { endOfLine: "auto" }, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }],
   },
 };
