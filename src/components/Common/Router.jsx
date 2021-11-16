20777dfb1033b2a63f19c37d74d0e032595b1c78
100Gyeon <bjy9966@naver.com>
2021-11-16 16:19:30 +0900

    chore: 불필요한 부분 삭제

--- src/components/Common/Router.jsx
+++ src/components/Common/Router.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Header';
 import Main from '../../pages/Main';
 import Sub from '../../pages/Sub';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/sub" element={<Sub />} />
