d8895b1dddeecb176c0e77d2eb5e47bb54675eac
100Gyeon <bjy9966@naver.com>
2021-11-16 21:39:16 +0900

    feat: Header, Billboard 구조화 및 스타일링

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
-import { Netflix_logo } from 'assets';
+import styled from 'styled-components';
+import { btn_bell, btn_more, btn_search, img_profile, Netflix_logo } from 'assets';
 
 const Header = () => {
   return (
-    <div>
-      <img src={Netflix_logo} />
-    </div>
+    <StyledHeader>
+      <StyledLeft>
+        <img src={Netflix_logo} />
+        <ul>
+          <li>홈</li>
+          <li>TV 프로그램</li>
+          <li>영화</li>
+          <li>NEW! 요즘 대세 콘텐츠</li>
+          <li>내가 찜한 콘텐츠</li>
+        </ul>
+      </StyledLeft>
+      <StyledRight>
+        <img src={btn_search} />
+        <img src={btn_bell} />
+        <img src={img_profile} />
+        <img src={btn_more} />
+      </StyledRight>
+    </StyledHeader>
   );
 };
 
 export default Header;
+
+const StyledHeader = styled.div`
+  position: fixed;
+  width: calc(100% - 72rem);
+  height: 5.2rem;
+  padding-left: 4.7rem;
+  padding-right: 3.9rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.8) 10%, rgba(0, 0, 0, 0));
+`;
+
+const StyledLeft = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > ul {
+    margin-left: 3.4rem;
+    display: flex;
+
+    & > li {
+      font-size: 1.2rem;
+      margin-right: 1.5rem;
+    }
+  }
+`;
+
+const StyledRight = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > img:first-child {
+    margin-right: 0.2rem;
+  }
+
+  & > img:nth-child(2) {
+    margin-right: 1.1rem;
+  }
+`;
--- src/components/Main/Billboard.jsx
+++ src/components/Main/Billboard.jsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
+import { img_header_main } from 'assets';
+import { img_header_title } from 'assets';
+import { ic_play } from 'assets';
+import { ic_info } from 'assets';
 
 const Billboard = () => {
-  return <div></div>;
+  return (
+    <StyledBillboard>
+      <StyledMainImg src={img_header_main} />
+      <StyledTitleImg src={img_header_title} />
+      <StyledBtnWrapper>
+        <button>
+          <img src={ic_play} />
+          <div>재생</div>
+        </button>
+        <button>
+          <img src={ic_info} />
+          <div>상세정보</div>
+        </button>
+      </StyledBtnWrapper>
+    </StyledBillboard>
+  );
 };
 
 export default Billboard;
+
+const StyledBillboard = styled.div``;
+
+const StyledMainImg = styled.img`
+  width: 100%;
+`;
+
+const StyledTitleImg = styled.img`
+  position: absolute;
+  top: 26.5rem;
+  left: 40rem;
+  width: 25.9rem;
+  height: 9.5rem;
+`;
+
+const StyledBtnWrapper = styled.div`
+  position: absolute;
+  top: 39.2rem;
+  left: 40rem;
+  display: flex;
+
+  & > button {
+    font-weight: 500;
+    font-size: 1.5rem;
+    border-radius: 2px;
+    padding: 1.1rem 2.3rem;
+    display: flex;
+    align-items: center;
+
+    & > img {
+      margin-right: 1rem;
+    }
+  }
+
+  & > button:first-child {
+    margin-right: 0.9rem;
+    background: #ffffff;
+  }
+
+  & > button:last-child {
+    background: #686868e5;
+    color: #ffffff;
+  }
+`;
