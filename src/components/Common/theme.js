4d943ef7bbaf24e6808b0a97749ac194ceed2a48
100Gyeon <bjy9966@naver.com>
2021-11-16 18:54:52 +0900

    feat: ThemeProvider 설정

--- src/components/Common/theme.js
+++ src/components/Common/theme.js
@@ -1,11 +1,13 @@
 const deviceSizes = {
   mobile: '360px',
   tablet: '768px',
+  laptop: '1920px',
 };
 
 const device = {
   mobile: `screen and (max-width: ${deviceSizes.mobile})`,
   tablet: `screen and (max-width: ${deviceSizes.tablet})`,
+  laptop: `screen and (max-width: ${deviceSizes.laptop})`,
 };
 
 const theme = {
