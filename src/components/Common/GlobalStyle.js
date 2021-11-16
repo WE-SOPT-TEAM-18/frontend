cc5f8e1cd26f48b5419d34de6dff70648e1cb6b0
100Gyeon <bjy9966@naver.com>
2021-11-16 21:40:38 +0900

    chore: GlobalStyle에 margin 값 추가 적용

--- src/components/Common/GlobalStyle.js
+++ src/components/Common/GlobalStyle.js
@@ -4,6 +4,19 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset};
 
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    margin: 0 36rem;
+  }
+
   html {
     font-size: 62.5%;
   }
