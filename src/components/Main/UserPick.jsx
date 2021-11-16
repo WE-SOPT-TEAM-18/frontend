38c8b7e5070702fba1f94636e64a08c50480dcdc
100Gyeon <bjy9966@naver.com>
2021-11-16 23:09:13 +0900

    feat: 시청중인 / 찜한 콘텐츠 구조화 및 스타일링

--- src/components/Main/UserPick.jsx
+++ src/components/Main/UserPick.jsx
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 const UserPick = () => {
-  return <div></div>;
+  const [current, setCurrent] = useState('2');
+  const [prev, setPrev] = useState(null);
+
+  const handleClick = (e) => {
+    setCurrent(e.target.id);
+  };
+
+  useEffect(() => {
+    if (current !== null) {
+      const currentBtn = document.getElementById(current);
+      currentBtn.style.color = '#d32f27';
+    }
+    if (prev !== null) {
+      const prevBtn = document.getElementById(prev);
+      prevBtn.style.color = '#ffffff';
+    }
+    setPrev(current);
+  }, [current]);
+
+  return (
+    <StyledUserPick>
+      <div>
+        <h2>박수아님이 시청중인 콘텐츠</h2>
+      </div>
+      <div>
+        <h2>내가 찜한 콘텐츠</h2>
+        <StyledBtnWrapper>
+          <button onClick={handleClick} id="1">
+            #시험 끝나면 볼 영화
+          </button>
+          <button onClick={handleClick} id="2">
+            #슬픈 영화
+          </button>
+          <button onClick={handleClick} id="3">
+            #슬기로운 시리즈
+          </button>
+          <button onClick={handleClick} id="4">
+            #마음의 평화
+          </button>
+          <button onClick={handleClick} id="5">
+            #무서운거
+          </button>
+          <button onClick={handleClick} id="6">
+            #음식
+          </button>
+        </StyledBtnWrapper>
+      </div>
+    </StyledUserPick>
+  );
 };
 
 export default UserPick;
+
+const StyledUserPick = styled.div`
+  font-size: 1.6rem;
+  font-weight: 700;
+  & > div {
+    margin-bottom: 3rem;
+    & > h2 {
+      margin-bottom: 0.8rem;
+    }
+  }
+`;
+
+const StyledBtnWrapper = styled.div`
+  display: flex;
+  & > button {
+    background: transparent;
+    color: #ffffff;
+    font-size: 1.6rem;
+    font-weight: 700;
+    padding: 0;
+    margin-right: 1.5rem;
+    margin-bottom: 0.8rem;
+  }
+  & > button.clicked {
+    color: #d32f27;
+  }
+`;
