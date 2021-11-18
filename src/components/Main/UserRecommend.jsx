e30ab073a289275eb5d7d0051a96ab1a965a2623
Hyoin-kim <hi980506@naver.com>
2021-11-18 22:11:59 +0900

    feat : 넷플릭스 영화 list 슬라이더 구현

--- src/components/Main/UserRecommend.jsx
+++ src/components/Main/UserRecommend.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import MovieData from './MovieData';
+import { arrow_left_gray, arrow_right_gray } from '../../assets/index';
 
 const RecommentWrapper = styled.div`
   margin: 0 auto;
   width: 1100px;
   overflow: hidden;
+
   .recommend {
     &__main {
     }
@@ -29,12 +31,27 @@ const RecommentWrapper = styled.div`
       position: absolute;
       padding-left: 55px;
     }
+    &__arrowLeft {
+      animation-fill-mode: forwards;
+      cursor: pointer;
+      z-index: 100;
+      float: left;
+      position: relative;
+      margin-top: 8px;
+    }
+    &__arrowRight {
+      animation-fill-mode: forwards;
+      z-index: 100;
+      cursor: pointer;
+      float: right;
+      position: relative;
+      margin-top: 1px;
+    }
   }
 `;
 
 const UserRecommend = () => {
   const [movies] = useState(MovieData);
-  // const [top10] = useState(Top10Data);
   return (
     <RecommentWrapper>
       <div className="recommend">
@@ -47,11 +64,62 @@ const UserRecommend = () => {
 };
 
 function Category(props) {
-  console.log(props.movies);
+  const totalSlide = 6;
+  const [scrollState, setScrollState] = useState(0);
+  const [animation, setAnimation] = useState(false);
+  const [localVisible, setLocalVisible] = useState(!scrollState);
+  const slideRef = useRef(null);
+
+  const prevButton = () => {
+    if (scrollState === 0) {
+      setScrollState(totalSlide);
+    } else {
+      setScrollState(scrollState - 1);
+    }
+  };
+
+  const nextButton = () => {
+    if (scrollState >= totalSlide) {
+      setScrollState(0);
+    } else {
+      setScrollState(scrollState + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (slideRef) {
+      document.getElementById(props.movies.id).style.transition = 'all 1s ease-in-out';
+      document.getElementById(props.movies.id).style.transform = `translateX(-${scrollState * 15}%)`;
+    }
+  }, [scrollState]);
+  useEffect(() => {
+    if ((localVisible && scrollState) || (localVisible && !scrollState)) {
+      setAnimation(true);
+      setTimeout(() => setAnimation(false), 500);
+    }
+    setLocalVisible(!scrollState);
+  }, [localVisible, scrollState]);
+
   return (
     <div className="recommend__main">
       <div className="recommend__contents">{props.movies.title}</div>
-      <div className="recommend__detail">
+      {(!localVisible || animation) && (
+        <img
+          className="recommend__arrowLeft"
+          src={arrow_left_gray}
+          onClick={() => {
+            prevButton();
+          }}
+        />
+      )}
+      <img
+        className="recommend__arrowRight"
+        src={arrow_right_gray}
+        onClick={() => {
+          nextButton();
+        }}
+      />
+      <div className="recommend__detail" id={props.movies.id}>
         {props.movies.imageNumber.map((a, i) => {
           return <MoviePost post={props.movies.imageNumber[i]} key={i} />;
         })}
@@ -64,7 +132,6 @@ function MoviePost(props) {
   return (
     <div className="recommend__movies">
       <img className="recommend__image" src={props.post.image} />
-      <img className="recommend__image" src={props.post.imageMovie} />
     </div>
   );
 }
