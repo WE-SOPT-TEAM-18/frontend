c606b3d572661a11dd3ba984e730b9b7ab46bbff
Ryammie <infiniteahn@gmail.com>
2021-11-19 22:51:53 +0900

    feat:sub 페이지 추가

---
+++ src/components/Sub/LikeData.jsx
@@ -0,0 +1,20 @@
+import { RectangleGray_like } from '../../assets/index';
+
+export default [
+  {
+    id: 0,
+    imageMovie: RectangleGray_like,
+  },
+  {
+    id: 1,
+    imageMovie: RectangleGray_like,
+  },
+  {
+    id: 2,
+    imageMovie: RectangleGray_like,
+  },
+  {
+    id: 3,
+    imageMovie: RectangleGray_like,
+  },
+];
--- src/pages/Sub.jsx
+++ src/pages/Sub.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from 'components/Common/Header';
+import LikeFiles from 'components/Sub/LikeFiles';
 import Footer from 'components/Common/Footer';
 
 const Sub = () => {
+  console.log('sub');
   return (
     <>
       <Header />
+      <LikeFiles />
       <Footer />
     </>
   );
