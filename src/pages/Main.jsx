5d74020ed7997d45eac7abc171c62bb2b149548f
Hyoin-kim <hi980506@naver.com>
2021-12-01 20:30:12 +0900

    chore : top8 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ import Billboard from 'components/Main/Billboard';
 import UserPick from 'components/Main/UserPick';
 import UserRecommend from 'components/Main/UserRecommend';
 import Footer from 'components/Common/Footer';
-import Top10List from 'components/Main/Top10List';
+import Top8List from 'components/Main/Top8List';
 
 const Main = () => {
   const [flag, setFlag] = useState(false);
@@ -13,7 +13,7 @@ const Main = () => {
       <Header />
       <Billboard />
       <UserPick flag={flag} setFlag={setFlag} />
-      <Top10List flag={flag} setFlag={setFlag} />
+      <Top8List flag={flag} setFlag={setFlag} />
       <UserRecommend flag={flag} setFlag={setFlag} />
       <Footer />
     </>
