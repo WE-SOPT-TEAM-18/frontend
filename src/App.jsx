89236c8cfd35a435b488e287773099d3aace2b57
Hyoin-kim <hi980506@naver.com>
2021-12-03 02:20:17 +0900

    chore : 불필요한 import 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components';
 import theme from 'components/Common/theme';
 import GlobalStyle from 'components/Common/GlobalStyle';
 import Router from 'components/Common/Router';
-import './index.css';
 
 function App() {
   return (
