f75533d759f4328ea6f2308637191aa742d788e5
100Gyeon <bjy9966@naver.com>
2021-12-23 02:32:29 +0900

    chore: 변수 상단으로 이동, css 수정

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { ic_all } from 'assets/index';
 const Footer = () => {
+  const Subtitle = `넷플릭스서비시스코리아 유한회사 통신판매업신고번호: 제2018 - 서울종로 - 0426호
+전화번호: 080-001-9587 
+대표: 레지널드 숀 톰프슨
+이메일 주소: korea @netflix.com 
+주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161
+사업자등록번호: 165-87-00119
+클라우드 호스팅: Amazon Web Services Inc.
+공정거래위원회 웹사이트`;
+
   return (
     <StyledRoot>
       <FooterIcon>
@@ -25,9 +34,7 @@ const Footer = () => {
           <li>문의 정보</li>
         </ul>
       </FooterList>
-      <FooterBtn>
-        <button>서비스 코드</button>
-      </FooterBtn>
+      <FooterDiv>서비스 코드</FooterDiv>
       <Description>{Subtitle}</Description>
     </StyledRoot>
   );
@@ -61,27 +68,21 @@ const FooterList = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-gap: 1.2rem;
+    @media ${({ theme }) => theme.device.mobile} {
+      grid-gap: 0.8rem;
+    }
   }
 `;
 
-const FooterBtn = styled.div`
-  font-size: 1rem;
-  color: none;
-  & > button {
-    color: #808080;
-    border: 1px solid #808080;
-    background-color: transparent;
-    padding: 0.6rem;
-  }
-
-  margin-bottom: 0.9rem;
+const FooterDiv = styled.div`
+  width: fit-content;
   margin-top: 2rem;
+  margin-bottom: 0.9rem;
+  font-size: 1rem;
+  color: #808080;
+  border: 1px solid #808080;
+  padding: 0.6rem;
 `;
-const Subtitle = `넷플릭스서비시스코리아 유한회사
-통신판매업신고번호: 제2018 - 서울종로 - 0426호 전화번호: 080-001-9587 
-대표:레지널드 숀 톰프슨 이메일 주소: korea @netflix.com 
-주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161
-사업자등록번호: 165-87-00119 클라우드 호스팅: Amazon Web Services Inc. 공정거래위원회`;
 
 const Description = styled.div`
   white-space: pre-wrap;
